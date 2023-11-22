df3647f5e34e4f3bcaa07cf3adadd28ec81e31ef
Bartol Deak <b@bdeak.net>
2023-11-22 20:10:09 +0100

    remove comment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,6 @@ sops -d "../ssh-keys/selfhosted.enc" | ssh-add -
 find "../services/$1" -name "*.enc.*" -exec sops -d -i {} \;
 
 rsync -av --progress "../services/$1" $user@$ip:services/
-# ssh $user@$ip "cd services/$1 && docker compose pull && docker compose up -d && docker system prune -af"
 
 ssh $user@$ip <<EOF
 cd services/$1 \
